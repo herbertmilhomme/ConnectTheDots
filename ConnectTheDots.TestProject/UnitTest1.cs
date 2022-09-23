bb28ba38933f9500cd333aebbdae4669050eefbe
Herb M <herbertmilhomme@gmail.com>
2022-09-23 02:22:27 -0400

    Update UnitTest1.cs

--- ConnectTheDots.TestProject/UnitTest1.cs
+++ ConnectTheDots.TestProject/UnitTest1.cs
@@ -1,13 +1,43 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConnectTheDots.Web;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ConnectTheDots.TestProject
 {
 	[TestClass]
 	public class UnitTest1
 	{
-		[TestMethod]
-		public void TestMethod1()
+		List<IncomingRequest> Actions = new List<IncomingRequest>
 		{
+			new IncomingRequest{ body = new Point { x = 0, y = 0 } }, //start
+			new IncomingRequest{ body = new Point { x = 2, y = 2 } }, //end
+			new IncomingRequest{ body = new Point { x = 2, y = 2 } }, //start
+			new IncomingRequest{ body = new Point { x = 2, y = 0 } }, //end
+			new IncomingRequest{ body = new Point { x = 2, y = 0 } }, //start
+			new IncomingRequest{ body = new Point { x = 0, y = 2 } }, //Creates an X, should throw invalid...
+			new IncomingRequest{ body = new Point { x = 0, y = 0 } }, 
+			new IncomingRequest{ body = new Point { x = 0, y = 3 } }, //creates a vertical line down 
+		};
+
+		[TestMethod]
+		public void TestLineSegmentCannotOverlap()
+		{
+			Game game = new Game();
+			game.Initialize();
+			OutgoingResponse arg = null;
+			foreach (IncomingRequest request in Actions)
+			{
+				if (request.msg == Game.NODE_CLICKED)
+				{
+					arg = game.Action(request.body);
+					//PayloadToJsonString(arg);
+				}
+			}
+			Assert.AreEqual(Game.INVALID_END_NODE, arg?.msg); //As long as last message is invalid 
 		}
 	}
 }
